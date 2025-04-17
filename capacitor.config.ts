cb6b8d1b70183a3e521b97862b0b0efde422df4b
bartolojet <bartolojeta@gmail.com>
2025-04-17 14:55:40 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mi rutina Fitness',
+  webDir: 'www'
+};
+
+export default config;
